7481d2805a64abd9a95171cf721654973cff8bbc
Piotr Duszyński <dooshek@desmart.com>
2024-05-21 20:19:36 +0200

    chore: init

---
+++ README.md
@@ -0,0 +1,83 @@
+
+# PyNonimizer - PostgreSQL Database Anonymizer
+
+This Python script is designed to anonymize specified fields in a PostgreSQL database using Faker to generate fake data. It is particularly useful in environments where sensitive data needs to be masked before sharing databases or datasets with third-party developers or for testing purposes.
+
+## Features
+
+- Anonymize specified fields across multiple tables.
+- Supports unique and primary key constraints for ensuring data integrity.
+- Allows ignoring specific records from being anonymized.
+- Configurable through a YAML definitions file for specifying which tables and fields to anonymize.
+- Ability to specify Faker locales for generating localized data.
+- Command-line arguments for database connection and configuration.
+
+## Prerequisites
+
+To run this script, you need:
+
+- Python 3.x
+- PostgreSQL database
+- Python packages: `psycopg2`, `faker`, `pyyaml`
+
+Ensure these are installed before proceeding. You can install the required Python packages using:
+
+```sh
+pip install -r requirements.txt
+```
+
+## Usage
+Prepare YAML Configuration Files:
+
+`defs.yaml`: Define the tables and fields you want to anonymize, along with the Faker methods to use for generating the fake data. Optionally, configure Faker locales.
+
+`ignores.yaml`: Specify any record IDs you wish to ignore and not anonymize.
+
+## Run the Script:
+
+Use the following command to run the script:
+
+```bash
+python anonymize.py --host <db_host> --port <db_port> --user <db_user> --name <db_name> --password <db_password> [--defs defs.yaml] [--ignores ignores.yaml] [--log-level info]
+```
+
+Replace <db_host>, <db_port>, <db_user>, <db_name>, and <db_password> with your database connection details.
+
+## Configuration Example
+
+`defs.yaml`
+```yaml
+faker:
+  locales: ["en_US"]
+tables:
+  users:
+    email: "email"
+    name: "name"
+
+
+`ignores.yaml`
+```yaml
+ignore_ids:
+  - 1
+  - 2
+```
+
+## Command-line Arguments
+
+`--host`: Database host.
+`--port`: Database port.
+`--user`: Database user.
+`--name`: Database name.
+`--password`: Database password.
+`--defs`: Path to the table definitions YAML file. Default is defs.yaml.
+`--ignores`: Path to the ignore list YAML file. Default is ignores.yaml.
+`--log-level`: Logging level (debug, info, warning, error, critical). Default is info.
+
+## Final note
+Ensure that the YAML configuration files accurately reflect your database schema.
+
+__Always create a backup of your database before running this script!__
+
+## License
+This project is open source and available under the MIT License.
+
---
+++ pynonymizer.py
@@ -0,0 +1,152 @@
+#!/bin/env python3
+import argparse
+from enum import unique
+import logging
+import time
+import faker
+import yaml
+import psycopg2
+from psycopg2 import extras
+from sys import exit
+from faker import Faker
+from psycopg2.errors import UniqueViolation
+
+# Setup argument parser
+parser = argparse.ArgumentParser()
+parser.add_argument("--host", required=True)
+parser.add_argument("--port", required=True)
+parser.add_argument("--user", required=True)
+parser.add_argument("--name", required=True)
+parser.add_argument("--password", required=True)
+parser.add_argument("--defs", default="defs.yaml")
+parser.add_argument("--ignores", default="ignores.yaml")
+parser.add_argument("--log-level", default="info", choices=["debug", "info", "warning", "error", "critical"])
+
+args = parser.parse_args()
+
+# Setup logging
+numeric_level = getattr(logging, args.log_level.upper(), None)
+logging.basicConfig(level=numeric_level)
+
+# Read table definitions and ignored IDs
+with open(args.defs, 'r') as file:
+    defs = yaml.safe_load(file)
+
+with open(args.ignores, 'r') as file:
+    ignored_ids = yaml.safe_load(file)["ignore_ids"]
+
+# Initialize Faker
+locales = defs.get('faker', {}).get('locales', ['en_US'])
+fake = Faker(locales)
+
+def process_table(table, fields, ignored_ids):
+    conn = psycopg2.connect(
+        dbname=args.name,
+        user=args.user,
+        password=args.password,
+        host=args.host,
+        port=args.port
+    )
+    conn.autocommit = False
+    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    
+    def get_unique_constraints(table_name):
+        cursor.execute(f"""
+            SELECT a.attname
+            FROM   pg_index i
+            JOIN   pg_attribute a ON a.attrelid = i.indrelid
+                                AND a.attnum = ANY(i.indkey)
+            WHERE  i.indrelid = '{table_name}'::regclass
+            AND    i.indisunique;
+        """)
+        results = cursor.fetchall()
+        if results is None:
+            return None
+        return [result[0] for result in results]
+        
+    def get_primary_key(table_name):
+        cursor.execute(f"""
+            SELECT a.attname
+            FROM   pg_index i
+            JOIN   pg_attribute a ON a.attrelid = i.indrelid
+                                AND a.attnum = ANY(i.indkey)
+            WHERE  i.indrelid = '{table_name}'::regclass
+            AND    i.indisprimary;
+        """)
+        result = cursor.fetchone()
+        if result is None:
+            return None
+        return result[0]
+
+    def update_record(table, pk, pk_value, field, value):
+        try:
+            cursor.execute(f"UPDATE {table} SET {field} = %s WHERE {pk} = %s", (value, pk_value))
+            return True
+        except UniqueViolation as e:
+            logging.warning(f"Duplicate key value violation for {field} with value {value}, {pk}={pk_value}. Regenerating...")
+            return False
+    
+    pk = get_primary_key(table)
+    unique_fields = get_unique_constraints(table)
+    
+    if pk is None:
+        logging.warning(f"Skipping table {table} due to missing primary key.")
+        cursor.close()
+        conn.close()
+        return
+    
+    cursor.execute(f"SELECT {pk}, {', '.join(fields.keys())} FROM {table}")
+    records = cursor.fetchall()
+    logging.info(f"Processing table {table}, total records: {len(records)}")
+    table_start_time = time.time()
+    updated_count = 0
+    faker_cache = {}
+    
+    for record in records:
+
+        if record[pk] in ignored_ids:
+            logging.warning(f"Skipping update for {table} with {pk} = {record[pk]} due to ignore list.")
+            continue
+
+        for field, faker_method in fields.items():
+            if not hasattr(fake, faker_method):
+                logging.error(f'Faker does not support the method: {faker_method}')
+                exit(1)
+            
+            value = getattr(fake, faker_method)()
+
+            # Field is unique, we need to check if the value has already been used and regenerate if necessary
+            if field in unique_fields:
+                while (value in faker_cache):
+                    value = getattr(fake, faker_method)()
+
+            faker_cache[value] = True
+
+            success = False
+            while not success:
+                try:
+                    success = update_record(table, pk, record[pk], field, value)
+                    if success:
+                        updated_count += 1
+                    else:
+                        value = getattr(fake, faker_method)()
+                except psycopg2.Error as e:
+                    logging.error(f"Error updating record: {e}")
+                    conn.rollback()
+                                            
+    # Commit the transaction
+    conn.commit()
+    logging.info(f"Processed {updated_count} records in {table}, time taken: {time.time() - table_start_time:.2f} seconds")
+    cursor.close()
+    conn.close()
+
+def main():
+    start_time = time.time()
+
+    for table, fields in defs["tables"].items():
+        process_table(table, fields, ignored_ids)
+
+    logging.info(f"Total time for script execution: {time.time() - start_time:.2f} seconds")
+    
+if __name__ == "__main__":
+    main()
